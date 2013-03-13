7b54ef9ac56ecf1dee81f48b7cbc3a3fa57fa025
Steffen Lyngbaek <steffenlyngbaek@gmail.com>
2013-03-12 17:58:09 -0700

    Added some animations and some error handling

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,19 @@
-$("form").submit(function(e) {
-    e.preventDefault(); // Prevents the page from refreshing
-    var $this = $(this); // `this` refers to the current form element
-    $.post(
-        $this.attr("action"), // Gets the URL to sent the post to
-        $this.serialize(), // Serializes form data in standard format
-        function(data) { /** code to handle response **/ },
-        "json" // The format the response should be in
-    );
+$(function(){  // $(document).ready shorthand
+   if ($('#score').length == 0) {
+      $('#score-card').hide();
+   }
+   else {
+      $('#score-card').css('visibility','visible').hide().fadeIn('slow');
+   }
+
+   // $("form").submit(function(e) {
+   //     e.preventDefault(); // Prevents the page from refreshing
+   //     var $this = $(this); // `this` refers to the current form element
+   //     $.post(
+   //         $this.attr("action"), // Gets the URL to sent the post to
+   //         $this.serialize(), // Serializes form data in standard format
+   //         function(data) { /** code to handle response **/ },
+   //         "json" // The format the response should be in
+   //     );
+   // });
 });
